dd1431a0cb0b2afc82e76874f13b75886ccc2df9
fernandezfran <fernandezfrancisco2195@gmail.com>
2021-11-30 17:26:46 -0300

    the code a little better

--- pyrrhenius/arrhenius.py
+++ pyrrhenius/arrhenius.py
@@ -1,73 +1,193 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
+
+"""Arrhenius fit, plot and extrapolation."""
 
 import matplotlib.pyplot as plt
+
+import numpy as np
+
+import pandas as pd
 
 import sklearn.linear_model
 
 
-def arrhenius(temperatures, dcoeff, plot=True, write_output=True):
-    """Arrhenius: Pasar a clase.
-    Recibe:
-      un np.array con las temperaturas y un diccionario con las temperaturas
-      como claves y pendientes del msd como valores, opcionalmente se le puede
-      pedir que plotee y guarde en 'difusion.png'
-    Devuelve:
-      el valor del coeficiente de difusión extrapolado a temperatura ambiente
-      con su error calculado a través de propagación de errores y considerando
-      la barra de error de cada punto (aproximada por la desviación estandar
-      del promedio) en el ajuste.
+class Arrhenius:
+    """Arrhenius class.
+
+    Parameters
+    ----------
+    temperatures, diffusion coefficient : array-like
+        (temperatures, diffusion coefficient) data.
+
+    temperr, differr : array-like
+        the error of each data point.
+
+    Notes
+    -----
+        Assume temperatures are in Kelvin and diffusion coefficients in cm^2/s.
     """
-    a2cm = 1e-8 ** 2
-    fs2s = 1e-15
-    units_change = a2cm / fs2s
 
-    tempinv = 1.0 / temperatures
-    diffli, difflierr = [], []
-    for temp in temperatures:
-        d = dcoeff[f"{temp}"].mean()
-        errd = dcoeff[f"{temp}"].std()
-
-        diffli.append(np.log(units_change * d))
-        difflierr.append(errd / d)
-    diffli, difflierr = np.array(diffli), np.array(difflierr)
-
-    reg = sklearn.linear_model.LinearRegression().fit(
-        tempinv.reshape(-1, 1), diffli, sample_weight=difflierr
-    )
-
-    # extrapolacion y propagación de errores
-    den = (
-        np.sum((tempinv / difflierr) ** 2) * np.sum(1 / difflierr ** 2)
-        - np.sum(tempinv / difflierr ** 2) ** 2
-    )
-    erra = np.sqrt(np.sum(1 / difflierr ** 2) / den)
-    errb = np.sqrt(np.sum((tempinv / difflierr) ** 2) / den)
-
-    d_amb = np.exp(reg.coef_[0] / 300 + reg.intercept_)
-    d_amb_err = d_amb * np.sqrt((erra / 300) ** 2 + errb ** 2)
-
-    if plot:
-        plt.xlabel(r"1 / T [K$^{-1}$]")
-        plt.ylabel(r"ln D [cm$^2$ / s]")
-        plt.ticklabel_format(style="sci", scilimits=(0, 0))
-        plt.errorbar(
-            tempinv, diffli, yerr=difflierr, marker="o", ls="", label="difusion"
+    def __init__(
+        self, temperatures, diffusion_coefficients, differr=None, temperr=None
+    ):
+        self.temperatures = np.asarray(temperatures, dtype=np.float32)
+        self.diffusion_coefficients = np.array(
+            diffusion_coefficients, dtype=np.float32
         )
-        plt.plot(tempinv, reg.coef_[0] * tempinv + reg.intercept_, label="ajuste")
-        plt.legend()
-        plt.savefig("arrhenius.png", dpi=600)
-        plt.show()
+        self.differr = differr
+        self.temperr = temperr
 
-    if write_output:
-        with open("arrhenius.dat", "w") as f_arr:
-            f_arr.write(
-                f"# log(diff) = {reg.coef_[0]:.6e} / T + {reg.intercept_:.6e}\n"
-            )
-            f_arr.write(f"# extrapola a: {d_amb:.6e} +/- {d_amb_err:.6e}\n")
-            f_arr.write(f"# tempinv, log(diff), log(differr)\n")
-            for t, d, de in zip(tempinv, diffli, difflierr):
-                f_arr.write(f"{t:.6e} {d:.6e} {de:.6e}\n")
+        # change to the variables ln(D) y 1/T
+        self.tempinv_ = 1 / self.temperatures
+        self.diff_ = np.log(self.diffusion_coefficients)
 
-    return d_amb, d_amb_err
+        # error propagation by partial derivatives:
+        self.tempinv_err_ = (
+            np.asarray(self.temperr, dtype=np.float32)
+            / (self.temperatures ** 2)
+            if self.temperr is not None
+            else None
+        )
+        self.diff_err_ = (
+            np.asarray(self.differr, dtype=np.float32)
+            / self.diffusion_coefficients
+            if self.differr is not None
+            else None
+        )
+
+    def _error_propagation(self, dtemp):
+        """Error of the extrapolation."""
+        delta = (
+            np.sum(1 / self.diff_err_ ** 2)
+            * np.sum((self.tempinv_ / self.diff_err_) ** 2)
+            - np.sum(self.tempinv_ / self.diff_err_ ** 2) ** 2
+        )
+        errslope = np.sqrt(np.sum(1 / self.diff_err_ ** 2) / delta)
+        errintercept = np.sqrt(
+            np.sum((self.tempinv_ / self.diff_err_) ** 2) / delta
+        )
+
+        return self.dcoeff_ * np.sqrt(
+            (errslope / dtemp) ** 2 + errintercept ** 2
+        )
+
+    def fit(self, **kwargs):
+        """Fit linear model.
+
+        Parameters
+        ----------
+        **kwargs
+            additional keyword arguments that are passed and are documented in
+            `sklearn.linear_model.LinearRegression`.
+
+        Returns
+        -------
+        tuple
+             a tuple with the fitted slope and intercept.
+        """
+        reg = sklearn.linear_model.LinearRegression(**kwargs).fit(
+            self.tempinv_.reshape(-1, 1),
+            self.diff_,
+            sample_weight=self.diff_err_,
+        )
+
+        self.slope_ = reg.coef_[0]
+        self.intercept_ = reg.intercept_
+
+        return self.slope_, self.intercept_
+
+    def extrapolate(self, dtemp=300.0):
+        """Extrapolates the diffusion coefficient at a desired temperature.
+
+        Parameters
+        ----------
+        dtemp : float, default=300.0
+            the desired temperature for the extrapolation.
+
+        Returns
+        -------
+        tuple
+            a tuple with the extrapolated diffusion coefficient at the desired
+            temperature as a first element and the respective error in the
+            second, if this was not possible to calculate, then is None.
+        """
+        self.dcoeff_ = np.exp(self.slope_ / dtemp + self.intercept_)
+        self.dcoefferr_ = (
+            self._error_propagation(dtemp)
+            if self.diff_err_ is not None
+            else None
+        )
+
+        return self.dcoeff_, self.dcoefferr_
+
+    def predict(self, temperatures):
+        """Predict using the linear model.
+
+        Parameters
+        ----------
+        temperatures : array-like
+            the temperatures at which you want to predict the diffusion
+            coefficient.
+        """
+        return self.intercept_ + self.slope_ * temperatures
+
+    def plot(self, ax=None, errorbar_kws=None, plot_kws=None):
+        """Arrhenius plot.
+
+        Parameters
+        ----------
+        ax : matplotlib.pyplot.Axes, default=None
+            the current axes.
+
+        errorbar_kws : dict, default=None
+            additional keyword arguments that are passed and are documented in
+            `matplotlib.pyplot.errorbar`.
+
+        plot_kws : dict, default=None
+            additional keyword arguments that are passed and are documented in
+            `matplotlib.pyplot.plot`.
+
+        Returns
+        -------
+        matplotlib.pyplot.Axes
+            The current axes.
+        """
+        ax = plt.gca() if ax is None else ax
+
+        errorbar_kws = {} if errorbar_kws is None else errorbar_kws
+        plot_kws = {} if plot_kws is None else plot_kws
+
+        for key, value in zip(
+            ["marker", "ls", "label"], ["o", "", "diffusion"]
+        ):
+            errorbar_kws.setdefault(key, value)
+
+        plot_kws.setdefault("label", "fit")
+
+        ax.errorbar(
+            self.tempinv_,
+            self.diff_,
+            yerr=self.diff_err_,
+            xerr=self.tempinv_err_,
+            **errorbar_kws,
+        )
+        ax.plot(self.tempinv_, self.predict(self.tempinv_), **plot_kws)
+
+        return ax
+
+    def to_csv(self, **kwargs):
+        """Write the results to a comma-separated values (csv) file.
+
+        Parameters
+        ----------
+        **kwargs
+            additional keyword arguments that are passed and are documented in
+            `pandas.DataFrame.to_csv`.
+        """
+        pd.DataFrame(
+            {
+                "temperatures-inv": self.tempinv_,
+                "extrapolated-diffusion-log": self.predict(self.tempinv_),
+            }
+        ).to_csv(**kwargs)
--- pyrrhenius/diffusion.py
+++ pyrrhenius/diffusion.py
@@ -1,29 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
 
-import matplotlib.pyplot as plt
+"""Function to fit the mean square displacement."""
+
+import numpy as np
 
 import sklearn.linear_model
 
 
 def get_diffusion_coefficient(
-        t, msd, start=0, stop=None, fit_intercept=False, dim=3
+    time, msd, start=0, stop=None, ndim=3, sample_weight=None, lr_kws=None
 ):
-    """To obtain the trace diffusion coefficient from MSD."""
-    stop = len(t) if stop is None else stop
+    """To obtain the trace diffusion coefficient from mean square displacement.
 
-    t = np.asarray(t, dtype=np.float32)
-    msd = np.asarray(msd, dtype=np.float32)
+    Parameters
+    ----------
+    time : array-like
+        time data.
 
-    X = t[start:stop].reshape(-1, 1)
-    y = msd[start:stop]
+    msd : array-like
+        mean square displacement data.
 
-    reg = sklearn.linear_model.LinearRegression(
-        fit_intercept=fit_intercept
-    ).fit(X, y)
+    start : int, default=0
+        the first value to be considered in the fit.
+
+    stop : int, default=None
+        the last value to be considered in the fit.
+
+    ndim : int, defualt=3
+        the dimension of the system.
+
+    sample_weight: array-like, default=None
+        as described in the method fit of
+        `sklearn.linear_model.LinearRegression`.
+
+    lr_kws : dict
+        additional keyword arguments that are passed and are documented in
+        `sklearn.linear_model.LinearRegression`.
+
+    Returns
+    -------
+    tuple
+        a tuple where the first element is a float with the fitted diffusion
+        coefficient and the second element is another tuple with floats,
+        the slope and the intercept of the fit.
+    """
+    stop = len(time) if stop is None else stop
+
+    lr_kws = {} if lr_kws is None else lr_kws
+
+    time = np.asarray(time, dtype=np.float32)[start:stop].reshape(-1, 1)
+    msd = np.asarray(msd, dtype=np.float32)[start:stop]
+
+    reg = sklearn.linear_model.LinearRegression(**lr_kws).fit(
+        time, msd, sample_weight
+    )
 
     slope = reg.coef_[0]
     intercept = reg.intercept_
+    dcoef = slope / (2 * ndim)
 
-    return slope / (2 * dim), (slope, intercept)
+    return dcoef, (slope, intercept)
