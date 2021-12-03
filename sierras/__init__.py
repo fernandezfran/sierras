2d852f84a35090d0a640dcfd6956dde265bdcf91
fernandezfran <fernandezfrancisco2195@gmail.com>
2021-12-03 14:23:04 -0300

    simple .gitignore and setup.py

---
+++ sierras/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of sierras (https://github.com/fernandezfran/sierras/).
+# Copyright (c) 2021, Francisco Fernandez
+# License: MIT
+#   Full Text: https://github.com/fernandezfran/sierras/blob/master/LICENSE
+
+# ============================================================================
+# DOCS
+# ============================================================================
+
+"""A module to perform arrhenius type plots for diffusion coefficients."""
+
+# ============================================================================
+# CONSTANTS
+# ============================================================================
+
+__author__ = "Francisco Fernandez"
+__email__ = "fernandezfrancisco2195@gmail.com"
+__version__ = "0.0.1"
+
+# use `pinta` as unit conversor
