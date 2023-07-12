1c5aee7bce46905fa84379a8c7d774aaff7ca689
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-07-12 15:29:00 -0300

    tests in a single file

--- test_sierras.py
+++ test_sierras.py
@@ -10,9 +10,139 @@
 # IMPORTS
 # ============================================================================
 
+from matplotlib.testing.decorators import check_figures_equal
+
 import numpy as np
 
+import pandas as pd
+
 import pytest
+
+import sierras
+
+# ============================================================================
+# TESTS
+# ============================================================================
+
+
+@pytest.mark.parametrize(("dataset"), ["fuller53", "desouza06", "weizhong08"])
+class TestArrheniusRegressor:
+    """Test the Arrhenius Regressor."""
+
+    def test_fit(self, dataset, request):
+        """Test the empirical Arrhenius equation fitting."""
+        dataset = request.getfixturevalue(dataset)
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        np.testing.assert_almost_equal(
+            areg.reg_.coef_[0], dataset["ref"]["slope"]
+        )
+        np.testing.assert_almost_equal(
+            areg.reg_.intercept_, dataset["ref"]["intercept"]
+        )
+
+    def test_predict(self, dataset, request):
+        """Test the prediction of the thermally-induced process."""
+        dataset = request.getfixturevalue(dataset)
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        pred = areg.predict(dataset["X"])
+
+        np.testing.assert_almost_equal(pred, dataset["ref"]["pred"])
+
+    def test_to_dataframe(self, dataset, request):
+        """Test the dataframe."""
+        dataset = request.getfixturevalue(dataset)
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        pred = areg.predict(dataset["X"])
+        df_ref = pd.DataFrame(
+            {
+                "temperatures": dataset["X"].ravel(),
+                "process": dataset["y"],
+                "process_pred": pred,
+            }
+        )
+        if dataset["sample_weight"] is not None:
+            df_ref["weights"] = dataset["sample_weight"]
+
+        df = areg.to_dataframe(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        assert df.equals(df_ref)
+
+    def test_activation_energy(self, dataset, request):
+        """Test the activation energy."""
+        dataset = request.getfixturevalue(dataset)
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        np.testing.assert_almost_equal(
+            areg.activation_energy_, dataset["ref"]["activation_energy"]
+        )
+
+    def test_extrapolation_at_room_temperature(self, dataset, request):
+        """Test the extrapolation at room temperature of the process."""
+        dataset = request.getfixturevalue(dataset)
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(
+            dataset["X"], dataset["y"], sample_weight=dataset["sample_weight"]
+        )
+
+        room_process = areg.predict(np.array([[300.00]]))
+
+        np.testing.assert_almost_equal(
+            room_process, dataset["ref"]["room_process"], decimal=16
+        )
+
+    @check_figures_equal(extensions=["png", "pdf"], tol=0.000001)
+    def test_plot(self, fig_test, fig_ref, dataset, request):
+        """Test the Arrhenius Plotter."""
+        dataset = request.getfixturevalue(dataset)
+
+        X, y, sample_weight = (
+            dataset["X"],
+            dataset["y"],
+            dataset["sample_weight"],
+        )
+
+        areg = sierras.ArrheniusRegressor()
+        areg.fit(X, y, sample_weight=sample_weight)
+        pred = areg.predict(dataset["X"])
+
+        # test
+        test_ax = fig_test.subplots()
+        areg.plot.arrhenius(X, y, ax=test_ax)
+
+        # expected
+        ref_ax = fig_ref.subplots()
+        ref_ax.errorbar(
+            1 / X,
+            np.log(y),
+            yerr=sample_weight / y if sample_weight is not None else None,
+            marker="o",
+            ls="",
+            label="data",
+        )
+        ref_ax.plot(1 / X, np.log(pred), label="fit")
+
 
 # =============================================================================
 # FIXTURES
